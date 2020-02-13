d5f7cf667f456c6480fb04093b21e5f635e22d17
igor <embiggenerd@gmail.com>
2020-02-13 15:16:31 -0500

    logger and invalidUserId mw done

--- server.js
+++ server.js
@@ -10,8 +10,35 @@ server.get('/', (req, res) => {
 
 function logger(req, res, next) {
   console.log(`
-    
+    req.method: ${req.method}
+    req.url: ${req.url}
+    timestamp: ${Date.now()}
     `)
+  next()
+}
+
+const validateUserId = async (req, res, next) => {
+  try {
+    const idCheck = await getById(req.params.id)
+
+    if(idCheck.length === 0 ) {
+      return res.status(400).json({messsage: "invalid user id"})
+    }
+    req.id = req.params.id
+    next()
+  }catch(e){
+    next(e)
+  }
+}
+
+const validateUser = async (req, res, next) => {
+  try {
+    if(!req.body){
+      
+    }
+  }catch(e) {
+    next(e)
+  }
 }
 
 module.exports = server;
